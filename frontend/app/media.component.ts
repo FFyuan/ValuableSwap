4eb090deb970d4c73a5fde0c5a167d5497697fac
FFyuan <tonyyuan127@gmail.com>
2016-04-28 15:50:07 -0400

    update

--- frontend/app/media.component.ts
+++ frontend/app/media.component.ts
@@ -10,7 +10,7 @@ import {Media} from './media';
     selector : 'media',
     template : `<div class="container" style="float : left; width : 25%;">
                 <ul class ="list-group" style="list-style-type: none; background-color: white;">
-                    <li class="list-item"><img src="http://www.computerhope.com/jargon/s/swap-file.jpg" style="width: 100%"> </li>
+                    <li class="list-item"><img src="http://www.hdums.com/img/swap.png" style="width: 100%"> </li>
                     <li class = "list-item">Category: {{media.category}}</li>
                     <li class = "list-item">Genre:{{media.genre}}</li>
                     <li class = "list-item">condition:{{media.condition}}</li>
