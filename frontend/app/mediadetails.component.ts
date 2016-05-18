739a7c1daf556837a480cdac44e0f20cdb111be7
FFyuan <tonyyuan127@gmail.com>
2016-05-17 20:44:42 -0400

    working

--- frontend/app/mediadetails.component.ts
+++ frontend/app/mediadetails.component.ts
@@ -5,13 +5,13 @@ import {Component, Input, OnInit, OnChanges, SimpleChange} from 'angular2/core';
 import {MediaService} from './media.service';
 import {Media} from './media';
 import {MediaComponent} from './media.component';
+import { RouteParams } from 'angular2/router';
 
 @Component({
 
         selector : 'mediadetails',
         template : `<div class="container-fluid" style="float: left; width: 300px">
                 <ul class ="well" style="list-style-type: none; ">
-                <div
                     <li class="list-item"><img src="http://www.hdums.com/img/swap.png" class="img-rounded" style="width: 100%"> </li>
                     <li class ="list-item">User : {{media.username}}></li>
                     <li class = "list-item">Category: {{media.category}}</li>
@@ -23,9 +23,18 @@ import {MediaComponent} from './media.component';
                     <li class = "list-item">Game System:{{media.game_system}}</li>
                 </ul>
                 </div>
-                `,
-
-
-
-
+                `
 })
+
+
+export class MediaDetailComponent{
+
+        media : Media;
+
+        constructor(private _routerPara : RouteParams, private _service : MediaService){}
+        ngOnInit(){
+                let id = +this._routerPara.get('id');
+                console.log(id);
+                this._service.getMediaById(id).subscribe(media => this.media = media);
+        }
+}
